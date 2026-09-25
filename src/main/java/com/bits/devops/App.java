bea3d755a77afad26dd375061c469e1e4c75d5ff
NIK86880​‌ <pandeyvayu3@gmail.com>
2026-09-25 10:21:47 +0530

    Initial commit - Maven project by Nikhil Pandey 2022WB86880

---
+++ src/main/java/com/bits/devops/App.java
@@ -0,0 +1,12 @@
+package com.bits.devops;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("=====================================");
+        System.out.println("DevOps Lab - Continuous Integration");
+        System.out.println("Name    : Nikhil Pandey");
+        System.out.println("BITS ID : 2022WB86880");
+        System.out.println("Build Version : 1");
+        System.out.println("=====================================");
+    }
+}
