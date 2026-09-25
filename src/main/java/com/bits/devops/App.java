4814ec5c7b478052642616a457ab72ef4280d5b3
NIK86880​‌ <pandeyvayu3@gmail.com>
2026-09-25 11:15:02 +0530

    Task 4 - Modified source code to demonstrate CI - Nikhil Pandey 2022WB86880

--- src/main/java/com/bits/devops/App.java
+++ src/main/java/com/bits/devops/App.java
@@ -6,7 +6,7 @@ public class App {
         System.out.println("DevOps Lab - Continuous Integration");
         System.out.println("Name    : Nikhil Pandey");
         System.out.println("BITS ID : 2022WB86880");
-        System.out.println("Build Version : 1");
+        System.out.println("Build Version : 2 - Modified for Continuous Integration");
         System.out.println("=====================================");
     }
 }
